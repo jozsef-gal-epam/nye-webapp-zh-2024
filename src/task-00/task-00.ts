c0f5386cc6adec9158f8ea44c8f0b3c12e291589
Bence <simon.bence45@gmail.com>
2024-03-18 13:28:44 +0100

    test "task-01"

--- src/task-00/task-00.ts
+++ src/task-00/task-00.ts
@@ -1,3 +1,4 @@
 export const oddsAndEvens = (num: number): 'odd' | 'even' => {
-  throw new Error('Not implemented');
+  const checkEvenOdd = (num: number): 'odd' | 'even' => num % 2 === 0 ? 'even' : 'odd';
+  return checkEvenOdd(num);
 };

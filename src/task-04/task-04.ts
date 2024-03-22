673a0812dd736680f264c48381f7a40ff5d1cb20
Ignéczi István <pisti2319@gmail.com>
2024-03-22 15:56:37 +0100

    .

--- src/task-04/task-04.ts
+++ src/task-04/task-04.ts
@@ -1,5 +1,44 @@
 import { Points, RPSInput, Shape, Outcome } from './models/rock-paper-scissors';
 
 export const rockPaperScissors = (gameSet: readonly RPSInput[]): number => {
-  throw new Error('Not implemented');
+  let sum = 0;
+
+  for (const i of gameSet) {
+
+    if(i.outcome === Outcome.DRAW) {
+      sum += Points.get(Outcome.DRAW) ?? 0;
+      sum += Points.get(i.shape) ?? 0;
+    }
+
+    if(i.outcome === Outcome.LOOSE) {
+      sum += Points.get(Outcome.LOOSE) ?? 0;
+    
+      if(i.shape === Shape.ROCK) {
+        sum += Points.get(Shape.SCISSORS) ?? 0;
+      }
+      if(i.shape === Shape.PAPER) {
+        sum += Points.get(Shape.ROCK) ?? 0;
+      }
+      if(i.shape === Shape.SCISSORS) {
+        sum += Points.get(Shape.PAPER) ?? 0;
+      }
+
+    }
+
+    if(i.outcome === Outcome.WIN) {
+      sum += Points.get(Outcome.WIN) ?? 0;
+    
+      if(i.shape === Shape.SCISSORS) {
+        sum += Points.get(Shape.ROCK) ?? 0;
+      }
+      if(i.shape === Shape.ROCK) {
+        sum += Points.get(Shape.PAPER) ?? 0;
+      }
+      if(i.shape === Shape.PAPER) {
+        sum += Points.get(Shape.SCISSORS) ?? 0;
+      }
+    }
+  }
+
+  return sum;
 };

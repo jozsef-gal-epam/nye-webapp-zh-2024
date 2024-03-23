042b6aae6a259bb2ec22e7b4e5ab576e213593d7
Ákos Balogh <akosbalogh16@gmail.com>
2024-03-23 13:00:01 +0100

    task-01, task-02, task-03, task-04, task-05

--- src/task-03/task-03.ts
+++ src/task-03/task-03.ts
@@ -1,5 +1,25 @@
 import { Draw, Game } from './models';
 
 export const minimalCubeSet = (games: Game[]): number => {
-  throw new Error('Not implemented');
+  let s = 0;
+
+  games.forEach(game => {
+    let red = 0, green = 0, blue = 0;
+
+    game.draws.forEach(draw => {
+      if (draw.red)
+        if (draw.red > red) red = draw.red
+
+      if (draw.green)
+        if (draw.green > green) green = draw.green
+
+      if (draw.blue)
+        if (draw.blue > blue) blue = draw.blue
+    });
+
+    s += (red * green * blue);
+  });
+
+
+  return s;
 };

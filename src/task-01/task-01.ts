8ecbf8dd287f75a54f0c92776b005ac6824e832e
Alex <fogialex01@gmail.com>
2024-03-23 23:15:58 +0100

    task-01, task-02, task-03, task-04, task-05

--- src/task-01/task-01.ts
+++ src/task-01/task-01.ts
@@ -1,5 +1,19 @@
 type Input = Array<any>;
 
 export const arraySum = (input: Input): number => {
-  throw new Error('Not implemented');
+  let sum = 0;
+
+  const traverse = (arr: Input) => {
+    arr.forEach(element => {
+      if (Array.isArray(element)) {
+        traverse(element);
+      } else if (typeof element === 'number'&& !isNaN(element)) {
+        sum += element;
+      }
+    });
+  };
+
+  traverse(input);
+
+  return sum;
 };
--- src/task-03/task-03.ts
+++ src/task-03/task-03.ts
@@ -1,5 +1,21 @@
 import { Draw, Game } from './models';
 
 export const minimalCubeSet = (games: Game[]): number => {
-  throw new Error('Not implemented');
+  return games.reduce((Sum, game) => {
+    
+    let maxR = 0, maxG = 0, maxB = 0;
+
+    game.draws.forEach(draw => {
+      
+      if (draw.blue && draw.blue > maxB) maxB = draw.blue;
+      if (draw.red && draw.red > maxR) maxR = draw.red;
+      if (draw.green && draw.green > maxG) maxG = draw.green;
+    });
+
+  
+    const amount = maxB* maxR * maxG  ;
+
+    
+    return Sum + amount;
+  }, 0); 
 };

41df08fc4a90d800d071ad1344f758150dc95980
jlaura23 <144628987+jlaura23@users.noreply.github.com>
2024-03-24 21:41:27 +0100

    Megoldott feladat task-03

--- src/task-03/task-03.ts
+++ src/task-03/task-03.ts
@@ -1,5 +1,14 @@
 import { Draw, Game } from './models';
 
 export const minimalCubeSet = (games: Game[]): number => {
-  throw new Error('Not implemented');
+  return games.reduce((totalStrength, game) => {
+    const gameStrength = game.draws.reduce((drawTotal, draw) => {
+      const redCount = draw.red || 0;
+      const greenCount = draw.green || 0;
+      const blueCount = draw.blue || 0;
+      const drawStrength = redCount * greenCount * blueCount;
+      return drawTotal + drawStrength;
+    }, 0);
+    return totalStrength + gameStrength;
+  }, 0);
 };

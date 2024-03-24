d0c3551614876703597b72706e00bddfce0e3647
jlaura23 <144628987+jlaura23@users.noreply.github.com>
2024-03-24 21:39:57 +0100

    Megoldott feladat task-01

--- src/task-01/task-01.ts
+++ src/task-01/task-01.ts
@@ -1,5 +1,21 @@
 type Input = Array<any>;
 
 export const arraySum = (input: Input): number => {
-  throw new Error('Not implemented');
+  let sum = 0;
+
+    const sumNumbers = (arr: any[]): void => {
+        for (const item of arr) {
+            if (typeof item === 'number' && !isNaN(item)) {
+                sum += item;
+            } else if (Array.isArray(item)) {
+                sumNumbers(item);
+            } else if (typeof item === 'object' && item !== null) {
+                sumNumbers(Object.values(item));
+            }
+        }
+    };
+
+    sumNumbers(input);
+
+    return sum;
 };

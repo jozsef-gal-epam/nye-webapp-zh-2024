c5d3f5816e9b201f90348bd6304c6e5f3c3ae788
Gergo202 <tgeri202@gmail.com>
2024-03-24 09:51:53 +0100

    osszes task, yarn.lock npm install

--- src/task-01/task-01.ts
+++ src/task-01/task-01.ts
@@ -1,5 +1,18 @@
 type Input = Array<any>;
 
 export const arraySum = (input: Input): number => {
-  throw new Error('Not implemented');
+  let total = 0;
+
+    const sumNumbers = (arr: Input) => {
+        for (const item of arr) {
+            if (Array.isArray(item)) {
+                sumNumbers(item);
+            } else if (typeof item === 'number' && !isNaN(item)) {
+                total += item;
+            }
+        }
+    };
+    
+    sumNumbers(input);
+    return total;
 };
--- src/task-05/task-05.ts
+++ src/task-05/task-05.ts
@@ -2,5 +2,47 @@ import { Movie, Genre, SearchParams, SearchResults, OrderBy, Direction } from '.
 import { MovieService } from './services';
 
 export const searchMovies = async (params: SearchParams): Promise<SearchResults> => {
-  return Promise.reject('Not implemented');
+  const { query, genre, orderBy = 'title', direction = 'ASC', offset = 0, limit = 12 } = params;
+
+  try {
+    const movies = await MovieService.getMovies();
+    let filteredMovies = movies;
+
+    if (query) {
+      const lowercaseQuery = query.toLowerCase(); 
+      filteredMovies = filteredMovies.filter(movie => 
+        movie.title.toLowerCase().includes(lowercaseQuery) || 
+        movie.overview.toLowerCase().includes(lowercaseQuery));
+    }
+
+    if (genre && genre.length > 0) {
+      filteredMovies = filteredMovies.filter(movie => 
+        movie.genres?.some(g => movie.genres?.includes(g)));
+    }
+
+    filteredMovies.sort((a, b) => {
+      const valueA = a[orderBy] || 0;
+      const valueB = b[orderBy] || 0;
+      if (typeof valueA === 'string' && typeof valueB === 'string') {
+        return direction === 'ASC' ? valueA.localeCompare(valueB) 
+          : valueB.localeCompare(valueA);
+      } else {
+        return direction === 'ASC' ? Number(valueA) - Number(valueB) 
+          : Number(valueB) - Number(valueA);
+      }
+    });
+
+    const slicedMovies = filteredMovies.slice(offset, offset + limit);
+
+    return {
+      total: filteredMovies.length,
+      movies: slicedMovies
+    };
+  } catch (error) {
+    console.error("Error occurred while fetching movies:", error);
+    return {
+      total: 0,
+      movies: []
+    };
+  }
 };

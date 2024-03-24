386c818fb6dc84fbf71a137bc5bbe30d06dc264c
jlaura23 <144628987+jlaura23@users.noreply.github.com>
2024-03-24 21:42:18 +0100

    Megoldott feladat task-04

--- src/task-04/task-04.ts
+++ src/task-04/task-04.ts
@@ -1,5 +1,18 @@
 import { Points, RPSInput, Shape, Outcome } from './models/rock-paper-scissors';
 
 export const rockPaperScissors = (gameSet: readonly RPSInput[]): number => {
-  throw new Error('Not implemented');
+  let totalPoints = 0;
+
+  for (const game of gameSet) {
+    const { shape, outcome } = game;
+
+    const opponentShape = shape === Shape.ROCK ? Shape.SCISSORS : shape === Shape.PAPER ? Shape.ROCK : Shape.PAPER;
+    const pointsFromOutcome = Points.get(outcome) ?? 0;
+
+    totalPoints += Points.get(shape) ?? 0;
+    totalPoints += Points.get(opponentShape) ?? 0;
+    totalPoints += pointsFromOutcome;
+  }
+
+  return totalPoints;
 };

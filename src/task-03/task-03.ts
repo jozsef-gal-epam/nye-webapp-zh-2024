4f9e976605ad70656edf265377be9b469ea5728f
Galata Noel <galatanoel@gmail.com>
2024-03-24 18:39:23 +0100

    megoldott feladatok: 0,1,2,3,4,5

--- src/task-03/task-03.ts
+++ src/task-03/task-03.ts
@@ -1,5 +1,22 @@
 import { Draw, Game } from './models';
 
 export const minimalCubeSet = (games: Game[]): number => {
-  throw new Error('Not implemented');
+  let totalStrength = 0;
+
+  for (const game of games) {
+     let minRed = Infinity;
+     let minGreen = Infinity;
+     let minBlue = Infinity;
+ 
+     for (const draw of game.draws) {
+       if (draw.red !== undefined) minRed = Math.min(minRed, draw.red);
+       if (draw.green !== undefined) minGreen = Math.min(minGreen, draw.green);
+       if (draw.blue !== undefined) minBlue = Math.min(minBlue, draw.blue);
+     }
+ 
+     const gameStrength = minRed * minGreen * minBlue;
+     totalStrength += gameStrength;
+  }
+
+  return totalStrength;
 };
--- src/task-04/task-04.ts
+++ src/task-04/task-04.ts
@@ -1,5 +1,15 @@
 import { Points, RPSInput, Shape, Outcome } from './models/rock-paper-scissors';
 
 export const rockPaperScissors = (gameSet: readonly RPSInput[]): number => {
-  throw new Error('Not implemented');
+  let totalPoints = 0;
+
+  for (const game of gameSet) {
+
+     totalPoints += Points.get(game.shape) || 0;
+ 
+
+     totalPoints += Points.get(game.outcome) || 0;
+  }
+ 
+  return totalPoints;
 };

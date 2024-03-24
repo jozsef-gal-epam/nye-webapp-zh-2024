1761a95fe2933263a804d757b8a99c832210529e
KovacsSebestyen <kovacsseb2002@gmail.com>
2024-03-24 21:31:30 +0100

    task-02, task-03, task-04, task-05

--- src/task-02/task-02.ts
+++ src/task-02/task-02.ts
@@ -4,5 +4,25 @@ export interface Item {
 }
 
 export const uniqueFilter = (array: Item[]): Item[] => {
-  throw new Error('Not implemented');
+  //throw new Error('Not implemented');
+
+  let out : Item[] = [];
+  let temp = true;
+  out.push(array[0]);
+  for(let i=0; i<array.length; i++)
+  {
+    temp = true;
+    for(let j=0; j<out.length; j++)
+    {
+      if(out[j].id == array[i].id)
+      {
+        temp = false;
+      }
+    }
+    if(temp)
+    {
+      out.push(array[i]);
+    }
+  }
+  return out;
 };
--- src/task-03/task-03.ts
+++ src/task-03/task-03.ts
@@ -1,5 +1,39 @@
 import { Draw, Game } from './models';
 
 export const minimalCubeSet = (games: Game[]): number => {
-  throw new Error('Not implemented');
+  //throw new Error('Not implemented');
+ 
+  let out = 0;
+  let red = 1;
+  let green = 1;
+  let blue = 1;
+  let temp : any = 0;
+
+  for (let i=0; i<games.length; i++)
+  {
+    red = 1;
+    green = 1;
+    blue = 1;
+    for(let j=0; j<games[i].draws.length; j++)
+    {
+      temp = games[i].draws[j].red? games[i].draws[j].red : 1;
+      if(temp>red)
+      {
+        red=temp;
+      }
+      temp = games[i].draws[j].green? games[i].draws[j].green : 1;
+      if(temp>green)
+      {
+        green=temp;
+      }
+      temp = games[i].draws[j].blue? games[i].draws[j].blue : 1;
+      if(temp>blue)
+      {
+        blue=temp;
+      }
+    }
+    out += (red*green*blue);
+  }
+
+  return out
 };

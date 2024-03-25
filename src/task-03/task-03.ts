dfdb0031be84f0835a455afe0b2757ea8e0aaff0
jlaura23 <144628987+jlaura23@users.noreply.github.com>
2024-03-25 06:20:38 +0100

    Megoldott feladat task-03

--- src/task-03/task-03.ts
+++ src/task-03/task-03.ts
@@ -3,10 +3,8 @@ import { Draw, Game } from './models';
 export const minimalCubeSet = (games: Game[]): number => {
   return games.reduce((totalStrength, game) => {
     const gameStrength = game.draws.reduce((drawTotal, draw) => {
-      const redCount = draw.red || 0;
-      const greenCount = draw.green || 0;
-      const blueCount = draw.blue || 0;
-      const drawStrength = redCount * greenCount * blueCount;
+      const { red = 0, green = 0, blue = 0 } = draw;
+      const drawStrength = red * green * blue;
       return drawTotal + drawStrength;
     }, 0);
     return totalStrength + gameStrength;

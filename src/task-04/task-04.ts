85d8c043fe7d2dfe2cd67fcb92ccf1648d9d0b0a
Heim János <63961444+heimjani@users.noreply.github.com>
2024-03-28 12:07:19 +0100

    Update task-04.ts

--- src/task-04/task-04.ts
+++ src/task-04/task-04.ts
@@ -1,5 +1,32 @@
 import { Points, RPSInput, Shape, Outcome } from './models/rock-paper-scissors';
 
 export const rockPaperScissors = (gameSet: readonly RPSInput[]): number => {
-  throw new Error('Not implemented');
+  return gameSet.reduce((totalPoints, game) => {
+    const ourShape = getOurShape(game.outcome, game.shape);
+    const movePoints = Points.get(ourShape) || 0;
+    const outcomePoints = Points.get(game.outcome) || 0;
+    return totalPoints + movePoints + outcomePoints;
+  }, 0);
+};
+
+function getOurShape(outcome: Outcome, opponentShape: Shape): Shape {
+  const winningMoves = {
+    [Shape.ROCK]: Shape.PAPER,
+    [Shape.PAPER]: Shape.SCISSORS,
+    [Shape.SCISSORS]: Shape.ROCK,
+  };
+  const losingMoves = {
+    [Shape.ROCK]: Shape.SCISSORS,
+    [Shape.PAPER]: Shape.ROCK,
+    [Shape.SCISSORS]: Shape.PAPER,
+  };
+
+  switch (outcome) {
+    case Outcome.WIN:
+      return winningMoves[opponentShape] || null;
+    case Outcome.LOOSE:
+      return losingMoves[opponentShape] || null;
+    default:
+      return opponentShape;
+  }
 };

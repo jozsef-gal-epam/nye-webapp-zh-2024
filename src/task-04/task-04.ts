9b360044816df73fe552a268cabbc202ea5a5bc8
kori521 <71168208+kori521@users.noreply.github.com>
2024-04-05 18:21:14 +0200

    G

--- src/task-04/task-04.ts
+++ src/task-04/task-04.ts
@@ -1,22 +1,49 @@
-import { RPSInput, Shape, Outcome } from './models/rock-paper-scissors';
+import { Points, RPSInput, Shape, Outcome } from './models/rock-paper-scissors';
 
 export const rockPaperScissors = (gameSet: readonly RPSInput[]): number => {
-  const shapePointsMap: Map<Shape, number> = new Map([
-    [Shape.ROCK, 1],
-    [Shape.PAPER, 2],
-    [Shape.SCISSORS, 3],
-  ]);
-
   let totalPoints = 0;
+  let pointsForShape = 0;
+  let pointsForOutcome = 0;
+  gameSet.forEach(game => {
+    const { shape, outcome } = game;
 
-  gameSet.forEach(({ shape, outcome }) => {
-    const ourPoints = shapePointsMap.get(shape) || 0;
-    if (outcome === Outcome.WIN) {
-      totalPoints += ourPoints + 6; // Add our points plus win points (6)
-    } else if (outcome === Outcome.DRAW) {
-      totalPoints += 3; // Add fixed points for a draw (3)
-    }
+    switch (Points.get(outcome)) {
+        case (0):  { //lose
+            pointsForOutcome = 0;
+            if(Points.get(shape) === 1){
+                pointsForShape = 3;
+            }
+            if(Points.get(shape) === 2){
+                pointsForShape = 1;
+            }
+            if(Points.get(shape) === 3){
+                pointsForShape = 2;
+            }
+            break;
+        }
+        case (3): { //draw
+            pointsForOutcome = 3;
+            pointsForShape = Points.get(shape) || 0;
+            break;
+        }
+        case (6): { //win
+            pointsForOutcome = 6;
+            if(Points.get(shape) === 1){
+                pointsForShape = 2;
+            }
+            if(Points.get(shape) === 2){
+                pointsForShape = 3;
+            }
+            if(Points.get(shape) === 3){
+                pointsForShape = 1;
+            }
+            break;
+        }
+            
+    }  
+    totalPoints += pointsForShape + pointsForOutcome;
   });
 
   return totalPoints;
 };
+

dc9c18f168fba7e2db5fb66c60b735b2fe6066ee
kori521 <71168208+kori521@users.noreply.github.com>
2024-04-05 17:20:04 +0200

    task2

--- src/task-02/task-02.ts
+++ src/task-02/task-02.ts
@@ -4,21 +4,17 @@ export interface Item {
 }
 
 export const uniqueFilter = (array: Item[]): Item[] => {
-  const uniqueMap = new Map<string, Item>();
+  const uniqueMap: Map<number, Item> = new Map();
 
-  array.forEach(obj => {
-    const key = `${obj.id}:${obj.name}`;
-    if (!uniqueMap.has(key)) {
-      uniqueMap.set(key, obj);
+  // Iterate through the input array
+  array.forEach(item => {
+    // Use the 'id' property as the key in the map
+    // Only add the item to the map if its 'id' is not already present
+    if (!uniqueMap.has(item.id)) {
+      uniqueMap.set(item.id, item);
     }
   });
 
-  const uniqueArray: Item[] = Array.from(uniqueMap.values());
-
-  // Check if there are more than 5 unique items, if so, return only the first 5
-  if (uniqueArray.length > 5) {
-    return uniqueArray.slice(0, 5);
-  }
-
-  return uniqueArray;
+  // Convert the map back to an array of items
+  return Array.from(uniqueMap.values());
 };

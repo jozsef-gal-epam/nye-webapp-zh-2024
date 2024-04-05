9a77e98ffda20bf2086ca8e0599028bef3b8444e
David <tremb.d98@gmail.com>
2024-04-05 13:16:13 +0200

    Task-01, Task-02, Task-04, Task-05

--- src/task-01/task-01.ts
+++ src/task-01/task-01.ts
@@ -1,5 +1,27 @@
 type Input = Array<any>;
 
 export const arraySum = (input: Input): number => {
-  throw new Error('Not implemented');
+  let sumOfArr = 0;
+
+  //tomb nem ures check
+  if (input.length === 0) {
+    return 0;
+  }else{
+
+    for (let i = 0; i < input.length; i++) {
+
+      if (typeof input[i] === 'number' && !isNaN(input[i])) {
+        sumOfArr += input[i];
+      }else if (Array.isArray(input[i])) {
+        sumOfArr += arraySum(input[i]);
+      }
+      
+    }
+  
+    return sumOfArr;
+  }
+
 };
+
+
+//KÉSZ 

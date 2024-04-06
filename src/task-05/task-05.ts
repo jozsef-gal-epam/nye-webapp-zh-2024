b30d7f0a1d05a7918dde0ec9b33e4d5ee570f7da
pecsi09 <117918990+pecsi09@users.noreply.github.com>
2024-04-06 23:01:31 +0200

    Update task-05.ts

--- src/task-05/task-05.ts
+++ src/task-05/task-05.ts
@@ -1,6 +1,55 @@
-import { Movie, Genre, SearchParams, SearchResults, OrderBy, Direction } from './models';
-import { MovieService } from './services';
+import { RPSInput, Shape, Outcome } from './models/rock-paper-scissors';
 
-export const searchMovies = async (params: SearchParams): Promise<SearchResults> => {
-  return Promise.reject('Not implemented');
+export const rockPaperScissors = (gameSet: readonly RPSInput[]): number => {
+  let sum = 0;
+  gameSet.forEach(element => {
+    switch (element.shape) {
+      case Shape.ROCK:
+        switch (element.outcome) {
+          case Outcome.WIN:
+            sum += 8; 
+            break;
+          case Outcome.LOOSE:
+            sum += 3; 
+            break;
+          case Outcome.DRAW:
+            sum += 4; 
+            break;
+        }
+        break;
+      case Shape.PAPER:
+        switch (element.outcome) {
+          case Outcome.WIN:
+            sum += 9; 
+            break;
+          case Outcome.LOOSE:
+            sum += 1; 
+            break;
+          case Outcome.DRAW:
+            sum += 5; 
+            break;
+        }
+        break;
+      case Shape.SCISSORS:
+        switch (element.outcome) {
+          case Outcome.WIN:
+            sum += 7;
+            break;
+          case Outcome.LOOSE:
+            sum += 2; 
+            break;
+          case Outcome.DRAW:
+            sum += 6; 
+            break;
+        }
+        break;
+    }
+  });
+  return sum;
 };
+
+console.log(rockPaperScissors([
+  { shape: Shape.ROCK, outcome: Outcome.DRAW },
+  { shape: Shape.PAPER, outcome: Outcome.LOOSE },
+  { shape: Shape.SCISSORS, outcome: Outcome.WIN },
+]));

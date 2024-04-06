5d7dc6922b2fb905d32f99be9552c6209433de1f
hu9angba <angelika99balogh@gmail.com>
2024-04-06 22:21:27 +0200

    Final commit

--- src/task-03/task-03.ts
+++ src/task-03/task-03.ts
@@ -1,5 +1,20 @@
 import { Draw, Game } from './models';
 
 export const minimalCubeSet = (games: Game[]): number => {
-  throw new Error('Not implemented');
+    if (!Array.isArray(games) || games.length === 0) {
+        return 0;
+    }
+
+    const totalStrength = games.reduce((total, game) => {
+        const gameStrength = game.draws.reduce((strength, draw) => {
+            const minRed = draw.red ?? 0;
+            const minGreen = draw.green ?? 0;
+            const minBlue = draw.blue ?? 0;
+            const drawStrength = minRed * minGreen * minBlue;
+
+            return strength + drawStrength;
+    }, 0); 
+        return total + gameStrength;
+    }, 0);
+    return totalStrength;
 };
--- src/task-04/task-04.ts
+++ src/task-04/task-04.ts
@@ -1,5 +1,32 @@
 import { Points, RPSInput, Shape, Outcome } from './models/rock-paper-scissors';
 
 export const rockPaperScissors = (gameSet: readonly RPSInput[]): number => {
-  throw new Error('Not implemented');
+  let points = 0;
+  
+  for (let i = 0; i < gameSet.length; i++) {
+    const { shape, outcome } = gameSet[i];
+
+    points += outcome === Outcome.WIN ? 6 + shapePoints(shape) :
+              outcome === Outcome.DRAW ? 3 + shapePoints(shape) :
+              outcome === Outcome.LOOSE ? 0 + shapePoints(shape) : 0;
+  }
+
+  return points + shapePointsTotal(gameSet);
+};
+
+const shapePoints = (shape: Shape): number => {
+  return shape === Shape.ROCK ? 1 :
+         shape === Shape.PAPER ? 2 :
+         shape === Shape.SCISSORS ? 3 : 0;
+};
+
+const shapePointsTotal = (gameSet: readonly RPSInput[]): number => {
+  let total = 0;
+
+  for (let i = 0; i < gameSet.length; i++) {
+    const { shape } = gameSet[i];
+    total += shapePoints(shape);
+  }
+
+  return total;
 };

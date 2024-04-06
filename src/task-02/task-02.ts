28004ffb3765f03ef17d1330a82fe20b148ebf3a
pecsi09 <117918990+pecsi09@users.noreply.github.com>
2024-04-06 22:59:41 +0200

    task-02.

--- src/task-02/task-02.ts
+++ src/task-02/task-02.ts
@@ -4,5 +4,28 @@ export interface Item {
 }
 
 export const uniqueFilter = (array: Item[]): Item[] => {
-  throw new Error('Not implemented');
+  const idSet = new Set<number>();
+  const output: Item[] = []
+  array.forEach(element=>{
+    if(!idSet.has(element.id)){
+        output.push(element)
+        idSet.add(element.id)
+    }
+
+    
+  })
+
+  return output;
 };
+
+console.log(
+  uniqueFilter([
+    { id: 1, name: 'a' },
+    { id: 2, name: 'b' },
+    { id: 4, name: 'c' },
+    { id: 5, name: 'd' },
+    { id: 4, name: 'c' },
+    { id: 2, name: 'b' },
+    { id: 6, name: 'e' },
+  ])
+);

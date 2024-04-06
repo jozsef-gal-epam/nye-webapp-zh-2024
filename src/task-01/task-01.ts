336fd00f70af1aba4cfcd166ded78b45106969af
hu9angba <angelika99balogh@gmail.com>
2024-04-06 19:20:32 +0200

    task-01

--- src/task-01/task-01.ts
+++ src/task-01/task-01.ts
@@ -1,20 +1,16 @@
 type Input = Array<any>;
 
+
 const calculateArraySum = (input: Input): number => {
   let sum = 0;
 
-  const sumArray = (arr: Input) => {
-    arr.forEach((item) => {
-      if (Array.isArray(item)) {
-        sumArray(item);
-      } else if (typeof item === 'number' && !isNaN(item)) {
-        sum += item;
-      }
-    });
-  };
-
-  sumArray(input);
+  input.forEach((item) => {
+    if (typeof item === 'number' && !isNaN(item)) {
+      sum += item;
+    } else if (Array.isArray(item)) {
+      sum += calculateArraySum(item);
+    }
+  });
 
   return sum;
 };
-

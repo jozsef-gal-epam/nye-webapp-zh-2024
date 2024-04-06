5b3fca6b1707e6cd5de6421a6cc5bbb6234b9eed
pecsi09 <117918990+pecsi09@users.noreply.github.com>
2024-04-06 22:58:55 +0200

    task-01

--- src/task-01/task-01.ts
+++ src/task-01/task-01.ts
@@ -1,5 +1,15 @@
 type Input = Array<any>;
 
 export const arraySum = (input: Input): number => {
-  throw new Error('Not implemented');
+    let sum = 0;
+    input.forEach(element => {
+        if (Array.isArray(element)) {
+            sum += arraySum(element);
+        } else if (typeof element === 'number'&& !isNaN(element)) {
+            sum += element;
+        }
+    });
+    return sum;
 };
+
+console.log(arraySum([1, 2, 3, [[[4], 5], [["6"]], { obj: 10 }]]));

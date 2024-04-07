5df7f93028c88f4b202f6a89c1db633aad055fa8
jrdani2001 <jrdani2001@gmail.com>
2024-04-08 01:14:01 +0200

    task-01, task-02, task-03, task-04, task-05

--- src/task-04/task-04.ts
+++ src/task-04/task-04.ts
@@ -1,5 +1,35 @@
 import { Points, RPSInput, Shape, Outcome } from './models/rock-paper-scissors';
 
 export const rockPaperScissors = (gameSet: readonly RPSInput[]): number => {
-  throw new Error('Not implemented');
+  let totalPoints = 0;
+
+  for (const { outcome, shape } of gameSet) {
+    const myShape = getMyShape(shape, outcome);
+    totalPoints += Points.get(myShape) || 0;
+    totalPoints += Points.get(outcome) || 0;
+  }
+
+  return totalPoints;
+};
+
+const getMyShape = (shape: Shape, outcome: Outcome): Shape => {
+  const shapeMap: Record<Outcome, Record<Shape, Shape>> = {
+    [Outcome.WIN]: {
+      [Shape.ROCK]: Shape.PAPER,
+      [Shape.PAPER]: Shape.SCISSORS,
+      [Shape.SCISSORS]: Shape.ROCK,
+    },
+    [Outcome.LOOSE]: {
+      [Shape.ROCK]: Shape.SCISSORS,
+      [Shape.PAPER]: Shape.ROCK,
+      [Shape.SCISSORS]: Shape.PAPER,
+    },
+    [Outcome.DRAW]: {
+      [Shape.ROCK]: Shape.ROCK,
+      [Shape.PAPER]: Shape.PAPER,
+      [Shape.SCISSORS]: Shape.SCISSORS,
+    },
+  };
+
+  return shapeMap[outcome][shape] || shape;
 };

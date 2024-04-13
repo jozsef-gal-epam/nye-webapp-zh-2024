9c6e52764fc80be7ba7b617f4cd91697545fed21
Kun Béla Roland <43463212+KBRoli@users.noreply.github.com>
2024-04-13 16:51:03 +0200

    Update task-02.ts

--- src/task-02/task-02.ts
+++ src/task-02/task-02.ts
@@ -1,8 +1,25 @@
-export interface Item {
+export interface Item 
+{
   id: number;
   name: string;
 }
 
-export const uniqueFilter = (array: Item[]): Item[] => {
-  throw new Error('Not implemented');
-};
+export const uniqueFilter = (array: Item[]): Item[] => 
+{
+  function uniqueItems(input: Item[]): Item[] 
+  {
+    const uniqueItems: Item[] = [];
+    const checkedIds: { [key: number]: boolean } = {};
+
+    for (const item of input) 
+    {
+        if (!checkedIds[item.id]) 
+          {
+            checkedIds[item.id] = true;
+            uniqueItems.push(item);
+          }
+    }
+    return uniqueItems;
+  }
+  return uniqueItems(array);
+}
